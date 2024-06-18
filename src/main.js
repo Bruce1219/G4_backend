40a162584329de4cd052c6140e10d1d97a602d6c
Bruce1219 <891219chiang@gmail.com>
2024-06-18 13:08:55 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import 'bootstrap/dist/css/bootsrap.min.css'
-import './assets/all.scss' //無法吃到變數，好像沒什麼用，目前寫在vite.config，先註解
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import './assets/all.scss' //無法吃到變數，好像沒什麼用，目前寫在vite.config，先註解
 import 'bootstrap'
 
 const app = createApp(App)
