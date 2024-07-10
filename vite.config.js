ad5cc992ab3d90386ebe214dc6e885e4e05d30ae
elaine0623 <0148004@gmail.com>
2024-07-10 20:46:12 +0800

    部署環境設定

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-    if (mode === 'prod') {
+export default defineConfig(({mode}) => {
+        // 放準備部署到server的設定
+        const env = loadEnv(mode, process.cwd(), '')
+        console.log('Mode:', mode)
+        console.log('ENV:', env)
         // 放準備部署到server的設定
         return {
             base: '/cid101/g4/admin/',
@@ -24,24 +27,32 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
                 alias: {
                     '@': fileURLToPath(new URL('./src', import.meta.url))
                 }
-            }
-        }
-    } else {
-        // 放原本的設定
-        return {
-            plugins: [vue(), VueDevTools()],
-            css: {
-                preprocessorOptions: {
-                    scss: {
-                        additionalData: `@import "@/assets/scss/style.scss";`
-                    }
-                }
             },
-            resolve: {
-                alias: {
-                    '@': fileURLToPath(new URL('./src', import.meta.url))
-                }
-            }
+            define: {
+            'import.meta.env.VITE_FILE_URL': JSON.stringify(env.VITE_FILE_URL),
+            'import.meta.env.VITE_ORIGIN_URL': JSON.stringify(env.VITE_ORIGIN_URL),
+            'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
         }
-    }
-})
+    } 
+})   
+    
+    
+//     else {
+//         // 放原本的設定
+//         return {
+//             plugins: [vue(), VueDevTools()],
+//             css: {
+//                 preprocessorOptions: {
+//                     scss: {
+//                         additionalData: `@import "@/assets/scss/style.scss";`
+//                     }
+//                 }
+//             },
+//             resolve: {
+//                 alias: {
+//                     '@': fileURLToPath(new URL('./src', import.meta.url))
+//                 }
+//             }
+//         }
+//     }
+// })
