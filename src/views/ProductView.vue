5d6a4ae6e0afeffbccecea315c0ea6d3a678e1cc
elaine0623 <0148004@gmail.com>
2024-07-11 16:08:40 +0800

    修改部署路徑

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -158,7 +158,7 @@ export default {
     methods: {
         async fetchProducts() {
             try {
-                const response = await fetch(`${import.meta.env.VITE_API_URL}product_api.php.php`)
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/product_api.php.php`)
                 //const response = await fetch('http://localhost/php_g4/product_api.php')
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`)
@@ -276,7 +276,7 @@ export default {
                 try {
                     const response = await fetch(
                         // `http://localhost/php_g4/product_api.php?id=${productId}`,
-                        `${import.meta.env.VITE_API_URL}/product_api.php?id=${productId}`,
+                        `${import.meta.env.VITE_API_URL}/product_api.php.php?id=${productId}`,
                         { method: 'DELETE' }
                     )
                     if (!response.ok) {
